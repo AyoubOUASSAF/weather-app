e3231a96b68eeb33a128fa40144656752c08b739
AyoubOUASSAF <133604856+AyoubOUASSAF@users.noreply.github.com>
2024-11-22 15:59:21 +0100

    Implement geolocation and city suggestions

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,20 @@ const app = express();
 const PORT = 5000;
 
 // Route to fetch weather data
-app.get("/api/weather", async (req, res) => {
-  const city = req.query.city;
+app.get("/api/suggested-cities", async (req, res) => {
+  const query = req.query.query;
   const apiKey = process.env.WEATHER_API_KEY;
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+  const apiUrl = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${query}`;
 
   try {
     const response = await axios.get(apiUrl);
-    res.json(response.data);
+    const cityNames = response.data.map((city) => city.name);
+    res.json(cityNames);
   } catch (error) {
-    res.status(500).json({ error: "Error fetching weather data" });
+    res.status(500).json({ error: "Unable to fetch suggested cities" });
   }
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:${PORT}");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
